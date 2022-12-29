fc38a441895c0ce941b755cd7c566f69a863a24f
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-12-29 17:03:53 +0000

    DTSPO-11790 - add expiresAfter tag

--- components/finops/inputs-default.tf
+++ components/finops/inputs-default.tf
@@ -5,3 +5,8 @@ variable "location" {
 variable "product" {
   default = "cft-platform"
 }
+
+variable "expiresAfter" {
+  description = "Date when Sandbox resources can be deleted. Format: YYYY-MM-DD"
+  default     = "3000-01-01"
+}
