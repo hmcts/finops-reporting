65aa2b80b08d48a4334ed0e92c554d093c70ea64
Thomas Thornton <>
2024-09-25 09:11:19 +0100

    add sbox local.log_analytics

--- components/finops/init.tf
+++ components/finops/init.tf
@@ -2,6 +2,9 @@ locals {
   log_analytics = {
     ptl = {
       subscription = "8999dec3-0104-4a27-94ee-6588559729d1"
+    },
+    sbox = {
+      subscription = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
     }
   }
 }
